48e0ebd0581dbf242d11397b49999f83662bd3bb
Fisher Adelakin <temidayoadelakin@gmail.com>
2013-12-01 13:47:56 -0500

    ready to deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,20 @@ LearnRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # email enabled in production
+  config.action_mailer.default_url_options = { :host => ENV["http://randapp.herokuapp.com/"] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["http://randapp.herokuapp.com/"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
